989c70d2715380fbac555294751d127bc7b2e631
niconunezz <niconuwii@gmail.com>
2026-02-12 22:56:17 +0100

    main dialect files

---
+++ include/clifford/Dialect/Clifford/IR/Dialect.h
@@ -0,0 +1,11 @@
+#ifndef CLIFFORD_DIALECT_CLIFFORD_IR_DIALECT_H
+#define CLIFFORD_DIALECT_CLIFFORD_IR_DIALECT_H
+
+#include "mlir/Interfaces/CallInterfaces.h"
+#include "mlir/Interfaces/FunctionInterfaces.h"
+#include "mlir/Interfaces/ControlFlowInterfaces.h"
+
+#include "mlir/IR/DialectImplementation.h"
+#include "clifford/Dialect/Clifford/IR/Dialect.h.inc"
+
+#endif
---
+++ lib/Dialect/Clifford/IR/Dialect.cpp
@@ -0,0 +1,18 @@
+
+#include "clifford/Dialect/Clifford/IR/Dialect.h"
+
+#include "clifford/Dialect/Clifford/IR/Dialect.cpp.inc"
+
+
+using namespace mlir::cliff;
+
+namespace mlir {
+namespace cliff {
+
+
+void CliffDialect::initialize() {
+    
+}
+
+} // end namespace mlir
+} // end namespace cliff
