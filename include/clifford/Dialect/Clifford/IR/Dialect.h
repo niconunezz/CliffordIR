e935728049a4ab09a15828acc6a4fa55cf05d252
niconunezz <niconuwii@gmail.com>
2026-02-15 22:22:08 +0100

    add funcOp, retOp, multivector and geo_prod

--- include/clifford/Dialect/Clifford/IR/Dialect.h
+++ include/clifford/Dialect/Clifford/IR/Dialect.h
@@ -2,6 +2,7 @@
 #define CLIFFORD_DIALECT_CLIFFORD_IR_DIALECT_H
 
 #include "mlir/Interfaces/CallInterfaces.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/FunctionInterfaces.h"
 #include "mlir/Interfaces/ControlFlowInterfaces.h"
 
--- lib/Dialect/Clifford/IR/Dialect.cpp
+++ lib/Dialect/Clifford/IR/Dialect.cpp
@@ -5,6 +5,8 @@
 #include "mlir/Dialect/UB/IR/UBOps.h"
 #include "llvm/ADT/StringSwitch.h"
 #include "llvm/ADT/TypeSwitch.h"
+#include "mlir/Interfaces/FunctionImplementation.h"
+
 
 #include "clifford/Dialect/Clifford/IR/Dialect.cpp.inc"
 
@@ -13,6 +15,32 @@ using namespace mlir::cliff;
 
 #define GET_TYPEDEF_CLASSES
 #include "clifford/Dialect/Clifford/IR/CliffTypes.cpp.inc"
+
+namespace mlir::cliff{
+
+void FuncOp::print(OpAsmPrinter &p) {
+    function_interface_impl::printFunctionOp(
+        p, *this, /*isVariadic=*/false, getFunctionTypeAttrName(),
+        getArgAttrsAttrName(), getResAttrsAttrName());
+    }
+    
+    
+ParseResult FuncOp::parse(OpAsmParser &parser, OperationState &result) {
+  auto buildFuncType =
+      [](Builder &builder, ArrayRef<Type> argTypes, ArrayRef<Type> results,
+         function_interface_impl::VariadicFlag,
+         std::string &) { return builder.getFunctionType(argTypes, results); };
+
+  return function_interface_impl::parseFunctionOp(
+      parser, result, /*allowVariadic=*/false,
+      getFunctionTypeAttrName(result.name), buildFuncType,
+      getArgAttrsAttrName(result.name), getResAttrsAttrName(result.name));
+}
+
+
+
+
+} // end namespace mlir::cliff
 
 
 void CliffDialect::initialize() {
