23027e8d15fe1845e43c107bbf880bc5101dbd22
niconunezz <niconuwii@gmail.com>
2026-02-18 21:17:09 +0100

    first draft of pythons API

---
+++ examples/screw_displacement.py
@@ -0,0 +1,20 @@
+import clifford as cl
+import torch
+
+
+@cl.compile
+def update_robot_arm_kernel(points_ptr, axis_start, axis_end, angle, distance):
+    axis = cl.Line(axis_start, axis_end)
+    motor = cl.Screw(line=axis, angle=angle, pitch=distance)
+
+    p = cl.load(points_ptr)
+
+    out = motor >> p
+    cl.store(points_ptr, out)
+
+
+def update_robot_arm():
+    # B, x, y, z 
+    points = ...
+
+    update_robot_arm_kernel[()](points)
