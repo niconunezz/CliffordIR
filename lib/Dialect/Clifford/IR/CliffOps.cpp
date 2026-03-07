57fb9910e97498c3713de3490b9be38f5616a024
niconunezz <niconuwii@gmail.com>
2026-03-07 17:10:46 +0100

    prepare geo_prod verifier

--- lib/Dialect/Clifford/IR/CliffOps.cpp
+++ lib/Dialect/Clifford/IR/CliffOps.cpp
@@ -3,4 +3,21 @@
 #define GET_OP_CLASSES
 #include "clifford/Dialect/Clifford/IR/CliffOps.cpp.inc"
 
+using namespace mlir::cliff;
+using namespace llvm;
 
+LogicalResult GeoProd::verify() {
+    auto LhsTy = dyn_cast<RankedTensorType>(getLhs().getType()).getElementType();
+    auto LhsMask = cast<Cliff_MultivectorType>(LhsTy).getMask();
+
+    auto RhsTy = cast<RankedTensorType>(getRhs().getType()).getElementType();
+    auto RhsMask = cast<Cliff_MultivectorType>(RhsTy).getMask();
+
+    auto OutTy = cast<RankedTensorType>(getOut().getType()).getElementType();
+    auto OutMask = cast<Cliff_MultivectorType>(OutTy).getMask();
+
+    if ((LhsMask ^ RhsMask) != OutMask) 
+        return emitError("Mask out should be xor(maskLHS, maskRHS) but isnt");
+
+    return success();
+}
