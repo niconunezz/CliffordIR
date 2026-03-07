a9fc98e98be839fbb01a5feeb1497929eb87357f
niconunezz <niconuwii@gmail.com>
2026-03-07 20:01:41 +0100

    ops

--- lib/Dialect/Clifford/IR/CliffOps.cpp
+++ lib/Dialect/Clifford/IR/CliffOps.cpp
@@ -16,8 +16,24 @@ LogicalResult GeoProd::verify() {
     auto OutTy = cast<RankedTensorType>(getOut().getType()).getElementType();
     auto OutMask = cast<Cliff_MultivectorType>(OutTy).getMask();
 
-    if ((LhsMask ^ RhsMask) != OutMask) 
-        return emitError("Mask out should be xor(maskLHS, maskRHS) but isnt");
+    // if ((LhsMask ^ RhsMask) != OutMask) 
+    //     return emitError("Mask out should be xor(maskLHS, maskRHS) but isnt");
+
+    return success();
+}
+
+LogicalResult Add::verify() {
+    auto LhsTy = dyn_cast<RankedTensorType>(getLhs().getType()).getElementType();
+    auto LhsMask = cast<Cliff_MultivectorType>(LhsTy).getMask();
+
+    auto RhsTy = cast<RankedTensorType>(getRhs().getType()).getElementType();
+    auto RhsMask = cast<Cliff_MultivectorType>(RhsTy).getMask();
+
+    auto OutTy = cast<RankedTensorType>(getOut().getType()).getElementType();
+    auto OutMask = cast<Cliff_MultivectorType>(OutTy).getMask();
+
+    if ((LhsMask | RhsMask) != OutMask) 
+        return emitError("Mask out should be or(maskLHS, maskRHS) but isnt");
 
     return success();
 }
