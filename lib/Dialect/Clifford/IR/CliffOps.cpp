a79f6f5f534260c6d0496bd17ce32a4047a20b36
niconunezz <niconuwii@gmail.com>
2026-03-09 17:47:43 +0100

    more ops

--- lib/Dialect/Clifford/IR/CliffOps.cpp
+++ lib/Dialect/Clifford/IR/CliffOps.cpp
@@ -37,3 +37,19 @@ LogicalResult Add::verify() {
 
     return success();
 }
+
+LogicalResult Sub::verify() {
+    auto LhsTy = dyn_cast<RankedTensorType>(getLhs().getType()).getElementType();
+    auto LhsMask = cast<Cliff_MultivectorType>(LhsTy).getMask();
+
+    auto RhsTy = cast<RankedTensorType>(getRhs().getType()).getElementType();
+    auto RhsMask = cast<Cliff_MultivectorType>(RhsTy).getMask();
+
+    auto OutTy = cast<RankedTensorType>(getOut().getType()).getElementType();
+    auto OutMask = cast<Cliff_MultivectorType>(OutTy).getMask();
+
+    if ((LhsMask | RhsMask) != OutMask) 
+        return emitError("Mask out should be or(maskLHS, maskRHS) but isnt");
+
+    return success();
+}
