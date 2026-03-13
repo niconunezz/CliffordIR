0f3f8559790264e533367cd2b7da0c93905424b4
niconunezz <niconuwii@gmail.com>
2026-03-13 17:42:05 +0100

    conversion base clases

---
+++ include/clifford/Dialect/CliffGPU/Transforms/CliffGPUConversion.h
@@ -0,0 +1,30 @@
+#ifndef CLIFFORD_DIALECT_CLIFFGPU_TRANSFORMS_CONVERSION_H
+#define CLIFFORD_DIALECT_CLIFFGPU_TRANSFORMS_CONVERSION_H
+
+#include "mlir/Transforms/DialectConversion.h"
+
+namespace mlir {
+
+class CliffGPUTypeConverter : public TypeConverter {
+
+public:
+    CliffGPUTypeConverter(MLIRContext *context, int numWarps, int threadsPerWarp);
+    int getNumWarps const { return getNumWarps; }
+    int getThreadsPerWarp const { return getthreadsPerWarpNumWarps; }
+
+private:
+    MLIRContext *context;
+    int numWarps;
+    int threadsPerWarp;
+};
+
+
+class CliffGPUConversionTarget : public ConversionTarget {
+
+public:
+    explicit CliffGPUConversionTarget(MLIRContext *ctx, const TypeConverter &typeConverter);
+}
+
+} // namespace mlir
+
+#endif
