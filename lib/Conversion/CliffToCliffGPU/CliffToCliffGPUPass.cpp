8447644c318ceb81c4e8f99396abcbe87b8ee77f
niconunezz <niconuwii@gmail.com>
2026-03-19 11:55:31 +0100

    add return type conversion

--- lib/Conversion/CliffToCliffGPU/CliffToCliffGPUPass.cpp
+++ lib/Conversion/CliffToCliffGPU/CliffToCliffGPUPass.cpp
@@ -1,8 +1,12 @@
 #include "clifford/Dialect/CliffGPU/IR/Dialect.h"
-#include "mlir/Transforms/DialectConversion.h"
-
 #include "clifford/Conversion/CliffToCliffGPU/Passes.h"
+
 #include "clifford/Dialect/Clifford/IR/Dialect.h"
+#include "clifford/Dialect/CliffGPU/Transforms/CliffGPUConversion.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "mlir/Pass/Pass.h"
+
+
 
 
 namespace mlir::cliff {
@@ -19,7 +23,35 @@ using namespace mlir::cliff;
 using namespace mlir::clg;
 
 
-struct 
+
+class ConvertCliffToCliffGPU : public cliff::impl::ConvertCliffToCliffGPUBase<ConvertCliffToCliffGPU> {
+
+public:
+
+using ConvertCliffToCliffGPUBase::ConvertCliffToCliffGPUBase;
+
+
+class CliffReturnPattern : public OpConversionPattern<Return> {
+public:
+    using OpConversionPattern::OpConversionPattern;
+
+    LogicalResult matchAndRewrite(Return op, Return::Adaptor adaptor,
+                                  ConversionPatternRewriter &rewriter) const override {
+        
+        rewriter.replaceOpWithNewOp<Return>(op, adaptor.getOperands());
+        return success();
+    } 
+};
+
+void runOnOperation() override {
+    MLIRContext *context = &getContext();
+    ModuleOp mod = getOperation();
+
+    CliffGPUTypeConverter typeConverter(context, numWarps, threadsPerWarp);
+    CliffGPUConversionTarget conversionTarget(*context, typeConverter);
+}
+
+};
 
 
 }
