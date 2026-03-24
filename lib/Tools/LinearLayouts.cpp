323dd94a9f6480ccbb76b0c2062ce3ac801c9b77
niconunezz <niconuwii@gmail.com>
2026-03-24 13:24:42 +0100

    getSupremum func

--- lib/Tools/LinearLayouts.cpp
+++ lib/Tools/LinearLayouts.cpp
@@ -48,6 +48,69 @@ bool operator==(const LinearLayout &lhs, const LinearLayout &rhs) {
     return LinearLayout(ret0, ret1);
 }
 
+SmallVector<StringAttr> getSupremum(const SmallVector<StringAttr> &a, const SmallVector<StringAttr> &b) {
+    SmallVector<StringAttr> ret;
+    const int stride = b.size() + 1;    
+    int m = a.size(), n = b.size();
+    std::vector<int16_t> lcs((m+1)*stride, 0);
+    for (auto i=0; i < a.size(); i++) {
+        for (auto j=0; j < b.size(); j++) {
+            lcs[(i + 1)*stride + j + 1] =
+                (a[i] == b[j]) ? lcs[i*stride + j] + 1 : max(lcs[(i+1)*stride + j], lcs[i*stride + j+1]);
+        }
+    }
+        
+    const int16_t lcs_size = lcs[a.size()*stride + b.size()];
+    // backtracing
+    std::vector<pair<int16_t, int16_t>> anchorPositions(lcs_size);
+    int16_t position = lcs_size;
+    while ((m > 0) && (n > 0)) {
+        if (a[m-1] == b[n-1]) {
+            anchorPositions[position-1] = std::make_pair<int16_t,int16_t>(m-1, n-1);
+            position--; m--; n--;
+        }
+        else if (lcs[(m-1)*stride + n] > lcs[m*stride + n-1]) {
+            m--;
+        }
+        else {
+            n--;
+        }
+    }
+    
+    int currIdxA = 0, currIdxB = 0;
+    for (const auto &[idxA, idxB] : anchorPositions) {
+        while (currIdxA != idxA) {
+            if (llvm::is_contained(ret, a[currIdxA]))
+                llvm_unreachable("Supremum does not exist");
+            
+            ret.push_back(a[currIdxA]);
+            currIdxA++;
+        }
+        while (currIdxB != idxB) {
+            if (llvm::is_contained(ret, b[currIdxB]))
+                llvm_unreachable("Supremum does not exist");
+            ret.push_back(b[currIdxB]);
+            currIdxB++;
+        }
+        ret.push_back(a[idxA]);
+    }
+
+    while (currIdxA != a.size()) {
+        if (llvm::is_contained(ret, a[currIdxA]))
+            llvm_unreachable("Supremum does not exist");
+        ret.push_back(a[currIdxA]);
+        currIdxA++;
+    }
+    while (currIdxB != b.size()) {
+        if (llvm::is_contained(ret, b[currIdxB]))
+            llvm_unreachable("Supremum does not exist");
+        ret.push_back(b[currIdxB]);
+        currIdxB++;
+    }
+
+    return ret;
+}
+
 LinearLayout operator*(LinearLayout inner, LinearLayout outer) {
     // for know we just need the case ll * empty = ll to work
     if (inner.getInDimNames().empty())
