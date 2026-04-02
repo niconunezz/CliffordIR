335688e6a1054ab4629190d72b31d87d8d55fd36
niconunezz <niconuwii@gmail.com>
2026-04-02 21:10:36 +0200

    typeConverter

---
+++ lib/Conversion/CliffGPUToLLVM/TypeConverter.cpp
@@ -0,0 +1,48 @@
+#include "mlir/Transforms/DialectConversion.h"
+
+
+#include "mlir/Dialect/UB/IR/UBOps.h"
+#include "mlir/IR/IRMapping.h"
+#include "mlir/Support/LLVM.h"
+
+#include "clifford/Conversion/CliffGPUToLLVM/Passes.h"
+#include "clifford/Dialect/Clifford/IR/Dialect.h"
+#include "clifford/Dialect/CliffGPU/IR/Dialect.h"
+#include "clifford/Conversion/CliffGPUToLLVM/TypeConverter.h"
+
+
+using namespace mlir;
+using namespace mlir::clg;
+
+
+
+
+CliffGPUToLLVMTypeConverter::CliffGPUToLLVMTypeConverter(MLIRContext *ctx) : context(ctx) {
+
+    addConversion([ctx](cliff::Cliff_MultivectorType mvector) {
+        int numComponents = __builtin_popcountll(mvector.getMask());
+        llvm::errs() << "numComponents : " << numComponents << "\n";
+        return LLVM::LLVMArrayType::get(ctx, mvector.getDtype(), numComponents);
+    });
+
+    addConversion([&](RankedTensorType type) {
+        auto ctx = type.getContext();
+        Type eltType = convertType(type.getElementType());
+        LinearEncodingAttr llAttr = dyn_cast<LinearEncodingAttr>(type.getEncoding());
+        assert(llAttr && "Encodings should always be linear encodings!");
+
+        //! this doesnt make sense rn, must think about
+        // unsigned elPerThread = llAttr.getTotalElemsPerThread(type.getShape());
+        SmallVector<Type, 4> types(1, eltType);
+        return LLVM::LLVMStructType::getLiteral(ctx, types);
+    });
+}
+
+CliffGPUToLLVMConversionTarget::CliffGPUToLLVMConversionTarget(MLIRContext &ctx,
+     const TypeConverter &typeConverter) : ConversionTarget(ctx) {
+
+    addLegalOp<ModuleOp>();
+    addLegalDialect<mlir::LLVM::LLVMDialect>();
+    addIllegalDialect<cliff::CliffDialect, clg::CliffGPUDialect>();
+
+}
