7d9a1da16a81bb15fa9b0d4c93f8d62c49088fa5
niconunezz <niconuwii@gmail.com>
2026-04-06 23:49:05 +0200

    first part of pattern, kind of working

--- lib/Conversion/CliffGPUToLLVM/CliffGPUToLLVMPass.cpp
+++ lib/Conversion/CliffGPUToLLVM/CliffGPUToLLVMPass.cpp
@@ -2,8 +2,11 @@
 #include "mlir/Pass/Pass.h"
 
 #include "clifford/Dialect/Clifford/IR/Dialect.h"
+
 #include "clifford/Dialect/CliffGPU/IR/Dialect.h"
 
+
+#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "clifford/Conversion/CliffGPUToLLVM/Passes.h"
 #include "clifford/Conversion/CliffGPUToLLVM/TypeConverter.h"
 
@@ -21,16 +24,128 @@ using namespace mlir;
 using namespace mlir::cliff;
 using namespace mlir::clg;
 
+struct ProductTerm {
+    int resultBit;
+    int lhsBit;
+    int rhsBit;
+    int sign;
+};
 
-// template <Class Op>
-// struct GenericOpPattern : public OpConversionPattern<FuncOp> {
-// public:
-//     using OpConversionPattern<Op>::OpConversionPattern;
 
-//     LogicalResult matchAndRewrite(Op op, Op::Adaptor adaptor, ConversionPatternRewriter &rewriter) const override {
-//         ...
-//     }
-// }
+
+class CliffGeoProdOpPattern : public OpConversionPattern<GeoProd> {
+public:
+    using OpConversionPattern::OpConversionPattern;
+    LogicalResult matchAndRewrite(GeoProd op, GeoProd::Adaptor adaptor, ConversionPatternRewriter &rewriter) const override { 
+        auto converter = getTypeConverter();
+        RankedTensorType lhsTensor = dyn_cast<RankedTensorType>(op.getLhs().getType());
+        RankedTensorType rhsTensor = dyn_cast<RankedTensorType>(op.getRhs().getType());
+        RankedTensorType outTensor = dyn_cast<RankedTensorType>(op.getOut().getType());
+        if (!lhsTensor || !rhsTensor || !outTensor)
+            return rewriter.notifyMatchFailure(op, "operands must be ranked tensors");
+        auto lhsTy = dyn_cast<Cliff_MultivectorType>(lhsTensor.getElementType());
+        auto rhsTy = dyn_cast<Cliff_MultivectorType>(rhsTensor.getElementType());
+        auto outTy = dyn_cast<Cliff_MultivectorType>(outTensor.getElementType());
+        if (!lhsTy || !rhsTy || !outTy)
+            return rewriter.notifyMatchFailure(op, "tensor elements must be multivector types");
+        
+        if (lhsTy.getSpace() != rhsTy.getSpace() || 
+            lhsTy.getSpace() != outTy.getSpace())
+            return rewriter.notifyMatchFailure(op, "all operands must belong to the same algebra");
+
+        auto algebra = dyn_cast<CliffordAlgebraAttr>(lhsTy.getSpace());
+        if (!algebra)
+            return rewriter.notifyMatchFailure(op, "algebra attribute must be a CliffordAlgebraAttr");
+
+        unsigned p = algebra.getP(), q = algebra.getQ(), r = algebra.getR();
+        uint64_t lhsMask = lhsTy.getMask();
+        uint64_t rhsMask = rhsTy.getMask();
+        uint64_t outMask = outTy.getMask();
+
+        // codegen
+        auto loc = op.getLoc();
+        auto fpTy = rewriter.getF32Type();
+        
+        auto adaptorLHS = adaptor.getLhs();
+        auto lhsMultivector= LLVM::ExtractValueOp::create(rewriter, loc, adaptorLHS, 0);
+        auto adaptorRHS = adaptor.getRhs();
+        auto rhsMultivector= LLVM::ExtractValueOp::create(rewriter, loc, adaptorRHS, 0);
+
+        SmallVector<Type> newResultTypes;
+        if (failed(converter->convertTypes(outTensor, newResultTypes)))
+            return rewriter.notifyMatchFailure(op, "failed to convert return type");
+
+        Type returnType = newResultTypes.empty()
+            ? LLVM::LLVMVoidType::get(getContext())
+            : newResultTypes[0];
+
+        // scalar case
+        if (!lhsMask || !rhsMask) {
+
+            auto cstMultivector = !lhsMask ? lhsMultivector : rhsMultivector;
+            auto dynMultivector = !lhsMask ? rhsMultivector : lhsMultivector;
+            auto dynMask = !lhsMask ? rhsMask : lhsMask;
+            auto cstScalar = LLVM::ExtractValueOp::create(rewriter, loc, cstMultivector, 0);
+                
+            while (dynMask) {
+                int dynIdx = __builtin_ctz(dynMask);
+                    
+                auto dynBasis = LLVM::ExtractValueOp::create(rewriter, loc, dynMultivector, dynIdx);
+                    
+                Value ret = arith::MulFOp::create(rewriter, loc, cstScalar, dynBasis);
+                    
+                LLVM::InsertValueOp::create(rewriter, loc, result, dynIdx )
+                dynMask &= dynMask - 1;
+            }
+            rewriter.eraseOp(op);
+            return success();
+        }
+
+
+        // SmallVector<ProductTerm> productTerms;
+        // while (lhsMask) {
+        //     int lhsBasis = __builtin_ctz(lhsMask);
+        //     while (rhsMask) {
+        //         int rhsBasis = __builtin_ctz(rhsMask);
+
+        //         int newBasis = lhsBasis ^ rhsBasis;
+        //         int newSign = reorderSign(lhsBasis, rhsBasis) * metricSign(lhsBasis, rhsBasis, p, q, r);
+        //         if (newSign != 0) {
+        //             ProductTerm term = {newBasis, lhsBasis, rhsBasis, newSign};
+        //             productTerms.push_back(term);
+
+        //         }
+                
+        //         rhsMask &= rhsMask - 1;
+        //     }
+
+        //     lhsMask &= lhsMask - 1;
+        // }
+
+        
+
+        // for (auto &term : productTerms) {
+        //     auto [newBasis, lhsBasis, rhsBasis, newSign] = term;
+        //     auto fpTy = rewriter.getF32Type();
+        //     auto lhs_i = rewriter.create<LLVM::ExtractValueOp>(loc, fpTy, adaptorLHSMask, ArrayRef<int64_t>{lhsBasis});
+        //     auto rhs_j = rewriter.create<LLVM::ExtractValueOp>(loc, fpTy, adaptorRHSMask, ArrayRef<int64_t>{rhsBasis});
+
+        //     Value sign_i = rewriter.create<arith::ConstantOp>(loc, newSign);
+        //     Value lhs_rhs = rewriter.create<arith::MulFOp>(loc, lhs_i, rhs_i);
+        //     Value result = rewriter.create<arith::MulFOp>(loc, lhs_rhs, sign_i);
+
+        //     rewriter.create<LLVM::InsertValueOp>(loc, result, component, )
+
+
+        // }
+
+
+        return success();
+
+
+    }
+
+};
 
 class CliffReturnOpPattern : public OpConversionPattern<ReturnOp> {
 public:
@@ -91,7 +206,7 @@ public:
 
 void populateCliffGPUToLLVMPatterns(TypeConverter &typeConverter, RewritePatternSet &patterns) {
     MLIRContext* context = patterns.getContext();
-    patterns.insert<CliffReturnOpPattern, CliffFuncOpPattern>(typeConverter, context);
+    patterns.insert<CliffReturnOpPattern, CliffFuncOpPattern, CliffGeoProdOpPattern>(typeConverter, context);
 }
 
 
